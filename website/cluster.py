37b407fd05724faefcc59733d29c552189b57f4e
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2024-02-27 21:43:12 +0000

    Added utilities file for constants and did styling
    I added a utilities file to hold some constants to follow best coding
    practices. I also did some styling so that now the buttons across the
    app look a little better and the survey page is looking a lot better,
    the questions above each set of radio buttons are now more like
    statements to match the answer options since making chnages to the
    dataset.
    
    Signed-off-by: Saoirse O'Donovan <saoirseodonovan13@gmail.com>

--- website/cluster.py
+++ website/cluster.py
@@ -1,39 +1,12 @@
 import numpy as np
 import pandas as pd
 from kmodes.kmodes import KModes
+from .utilities import questions
 
 # For assistance with KModes usage: https://pypi.org/project/kmodes/ Accessed January 11, 2024. 
 
 #adding a random seed ensures that the attributes associated with each cluster will not change each time that the code is run 
 np.random.seed(42)
-
-#sample questions
-questions = [
-    'Is it a dealbreaker if your partner is open to polyamorous relationships? ',
-    'Is it a dealbreaker if your partner has a child/children? ',
-    'Is it a dealbreaker if your partner does not want a child/children? ',
-    'Is it a dealbreaker if your partner does not take care of themselves? ',
-    'Is it a dealbreaker if your partner is not athletic? ',
-    'Is it a dealbreaker if your partner is overly athletic? ',
-    'Is it a dealbreaker if your partner abuses substances? ',
-    'Is it a dealbreaker if your partner lives more than 3 hours from you? ',
-    'Is it a dealbreaker if your partner is lazy?',
-    'Is it a dealbreaker if your partner is unambitious?',
-    'Is it a dealbreaker if your partner is clingy?',
-    'Is it a dealbreaker if your partner is arrogant?',
-    'Is it a dealbreaker if your partner has poor personal hygiene?',
-    'Is it a dealbreaker if your partner is a poor communicator?',
-    'Is it a dealbreaker if your partner is inconsistent?',
-    'Is it a dealbreaker if your partner tends to be disrespectful to others?',
-    'Is it a dealbreaker if your partners romantic behaviours differ from yours? e.g. differing love languages, methods of expressing romatic feelings. ',
-    'Is it a dealbreaker if your partner wishes to monitor your online activity? e.g. looking though text messages, content watched, social media posts and communications.  ',
-    'Is it a dealbreaker if your partner is secretive about their own online activity? ',
-    'Is it a dealbreaker if your partner suffers from financial strain?',
-    'Is it a dealbreaker if your partner has not completed their high school/secondary school studies?',
-    'Is it a dealbreaker if your partner has not attended university or an equivalent third level institute?',
-    'Is it a dealbreaker if your partner suffers from mental health issues?',
-    'Is it a dealbreaker if your partner suffers from health problems?',
-]
 
 #get data from generated, expanded dataset
 data = pd.read_csv('generated-new.csv', header=None)
---
+++ website/utilities.py
@@ -0,0 +1,60 @@
+#constants are to be stored here
+
+#move seed here
+
+#questions
+#sample questions
+questions = [
+    'Is it a dealbreaker if your partner is open to polyamorous relationships? ',
+    'Is it a dealbreaker if your partner has a child/children? ',
+    'Is it a dealbreaker if your partner does not want a child/children? ',
+    'Is it a dealbreaker if your partner does not take care of themselves? ',
+    'Is it a dealbreaker if your partner is not athletic? ',
+    'Is it a dealbreaker if your partner is overly athletic? ',
+    'Is it a dealbreaker if your partner abuses substances? ',
+    'Is it a dealbreaker if your partner lives more than 3 hours from you? ',
+    'Is it a dealbreaker if your partner is lazy?',
+    'Is it a dealbreaker if your partner is unambitious?',
+    'Is it a dealbreaker if your partner is clingy?',
+    'Is it a dealbreaker if your partner is arrogant?',
+    'Is it a dealbreaker if your partner has poor personal hygiene?',
+    'Is it a dealbreaker if your partner is a poor communicator?',
+    'Is it a dealbreaker if your partner is inconsistent?',
+    'Is it a dealbreaker if your partner tends to be disrespectful to others?',
+    'Is it a dealbreaker if your partners romantic behaviours differ from yours? e.g. differing love languages, methods of expressing romatic feelings. ',
+    'Is it a dealbreaker if your partner wishes to monitor your online activity? e.g. looking though text messages, content watched, social media posts and communications.  ',
+    'Is it a dealbreaker if your partner is secretive about their own online activity? ',
+    'Is it a dealbreaker if your partner suffers from financial strain?',
+    'Is it a dealbreaker if your partner has not completed their high school/secondary school studies?',
+    'Is it a dealbreaker if your partner has not attended university or an equivalent third level institute?',
+    'Is it a dealbreaker if your partner suffers from mental health issues?',
+    'Is it a dealbreaker if your partner suffers from health problems?',
+]
+
+#survey page statements
+surveyQuestions = [
+    "It's a dealbreaker if my partner is open to having a polyamorous relationship.",
+    "It's a dealbreaker if my partner has a child/children.",
+    "It's a dealbreaker if my partner does not want a child/children.",
+    "It's a dealbreaker if my partner does not take care of themselves.",
+    "It's a dealbreaker if my partner is not athletic.",
+    "It's a dealbreaker if my partner is overly athletic.",
+    "It's a dealbreaker if my partner abuses substances.",
+    "It's a dealbreaker if my partner lives more than 3 hours away from me.",
+    "It's a dealbreaker if my partner is lazy.",
+    "It's a dealbreaker if my partner is unambitious.",
+    "It's a dealbreaker if my partner is clingy.",
+    "It's a dealbreaker if my partner is arrogant.",
+    "It's a dealbreaker if my partner has poor personal hygiene.",
+    "It's a dealbreaker if my partner is a poor communicator.",
+    "It's a dealbreaker if my partner is inconsistent.",
+    "It's a dealbreaker if my partner is disrespectful to others.",
+    "It's a dealbreaker if my partners romantic behaviours differ from mine. e.g. differing love languages, methods of expressing romatic feelings.",
+    "It's a dealbreaker if my partner wishes to monitor my online activity. e.g. looking though text messages, content watched, social media posts and communications.",
+    "It's a dealbreaker if my partner is secretive about their own online activity.",
+    "It's a dealbreaker if my partner suffers from financial strain.",
+    "It's a dealbreaker if my partner has not completed their second level studies.",
+    "It's a dealbreaker if my partner has not attended university or an equivalent third level institute.",
+    "It's a dealbreaker if my partner suffers from mental health issues.",
+    "It's a dealbreaker if my partner suffers from health problems.",
+]
