88715220cb14c9ddc6afe7e3b724341fd50e39c7
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2024-03-03 22:34:25 +0000

    Email now sent to user with cluster result
    During the semester one fyp demo, this was a suggested addition to make
    to the application so that users are able to go back and check when they
    last took the survey and to reflect on older categories they had been
    placed in. So I have set up an app password for my google account and
    that is used to send emails to the emails of the current logged in user
    when they click the 'submit' button on the survey.html page.
    
    Signed-off-by: Saoirse O'Donovan <saoirseodonovan13@gmail.com>

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,9 @@
 #For assistance with flask sign up, login and database (SQLAlchemy) usage: https://youtu.be/uZnp21fu8TQ?si=i165JQRHuDr-G8hO Accessed December 13, 2023.
-from flask import Flask
+#For assistance with setting up mail configurations: https://www.youtube.com/watch?v=nOkpTwPvDTg Accessed March 3, 2024.
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 import os
 
 #initialize SQLAlchemy object
@@ -14,16 +16,24 @@ def create_app():
     app = Flask(__name__ , static_folder="static")
     app.config['SECRET_KEY'] = 'a402cd29cade63e402342cb2'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['MAIL_SERVER'] = "smtp.googlemail.com"
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'noreply.earlydays@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'duik xyxf kxho atgj'
     app.config['MAX_CONTENT_PATH'] = 26214400
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     db.init_app(app)
 
-    from .views import views
+    from .views import views, setup_views
     from .auth import auth
     from .models import User
 
+    mail = Mail(app)
+    app.register_blueprint(setup_views(mail), url_prefix='/')
+
     #views and auth are blueprint objects
-    app.register_blueprint(views, url_prefix='/')
+    # app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     with app.app_context():
