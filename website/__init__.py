50320b79d4d498f34ea192ad54fea8d45750b6fa
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2024-03-05 18:49:08 +0000

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,12 @@
 #For assistance with flask sign up, login and database (SQLAlchemy) usage: https://youtu.be/uZnp21fu8TQ?si=i165JQRHuDr-G8hO Accessed December 13, 2023.
 #For assistance with setting up mail configurations: https://www.youtube.com/watch?v=nOkpTwPvDTg Accessed March 3, 2024.
+#For assistance with deployment: https://www.youtube.com/watch?v=IBfj_0Zf2Mo Accessed March 5, 2024.
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as sa
 from flask_login import LoginManager
 from flask_mail import Mail, Message
 import os
-
-secret_key = os.environ.get('secret_key')
-mail_password = os.environ.get('mail_password')
-mail_username = os.environ.get('mail_username')
 
 #initialize SQLAlchemy object
 db = SQLAlchemy()
@@ -20,15 +17,12 @@ path = os.getcwd()
 def create_app():
     app = Flask(__name__ , static_folder="static")
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['MAIL_SERVER'] = "smtp.googlemail.com"
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-    # app.config['MAX_CONTENT_PATH'] = 26214400
-    # app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     db.init_app(app)
 
     from .views import views, setup_views
@@ -39,7 +33,6 @@ def create_app():
     app.register_blueprint(setup_views(mail), url_prefix='/')
 
     #views and auth are blueprint objects
-    # app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     with app.app_context():
@@ -55,15 +48,15 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
     
-    #https://testdriven.io/blog/flask-render-deployment/
+    #Fixed issue in __init__.py of database tables not being created during deployment: https://testdriven.io/blog/flask-render-deployment/ Accessed March 5, 2024.
     engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
     inspector = sa.inspect(engine)
     if not inspector.has_table("users"):
         with app.app_context():
             db.create_all()
-            app.logger.info('Initialized the database!')
+            app.logger.info('Database initialised with user table.')
     else:
-        app.logger.info('Database already contains the users table.')
+        app.logger.info('User table already in database.')
 
 
     return app
