9d38415a531897d9d0cf86f2946e308ec9e6146e
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2024-03-05 17:14:06 +0000

    Debugging environment variable issues
    Signed-off-by: Saoirse O'Donovan <saoirseodonovan13@gmail.com>

--- website/__init__.py
+++ website/__init__.py
@@ -22,8 +22,8 @@ def create_app():
     app.config['MAIL_SERVER'] = "smtp.googlemail.com"
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = mail_username
-    app.config['MAIL_PASSWORD'] = mail_password
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     # app.config['MAX_CONTENT_PATH'] = 26214400
     # app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     db.init_app(app)
