e85da5ca26b942bdbcc8af1a0bea9e47b976e12a
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2024-03-13 18:28:12 +0000

    Fixed issue with broken flash close button
    The close (x) button on the flash messages were not operational so I
    added some JQuery to fix this. I also fixed the navbar up based on some
    feedback I got from my site feedback google form which suggested not
    having the 'signup' and #login' options when signed in.
    
    Signed-off-by: Saoirse O'Donovan <saoirseodonovan13@gmail.com>

--- website/auth.py
+++ website/auth.py
@@ -19,7 +19,6 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Login was successful!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.welcome'))
             else:
